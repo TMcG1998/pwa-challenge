782ad5bed4146b2eae0841eddeb160abd58e69d0
Tommy McG <thomasjmcgovern98@gmail.com>
2022-07-01 17:30:04 -0400

    added web manifest and service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,18 +4,78 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     "./index.html",
-    "./events.html",
-    "./tickets.html",
-    "./schedule.html",
-    "./assets/css/style.css",
-    "./assets/css/bootstrap.css",
-    "./assets/css/tickets.css",
-    "./dist/app.bundle.js",
-    "./dist/events.bundle.js",
-    "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js"
+    "./js/idb.js",
+    "./js/index.js",
+    "./css/styles.css",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
   ];
 
 self.addEventListener('install', function (e) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were pre-cached successfully!');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    )
+    self.skipWaiting();
+})
 
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function(evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if(key != CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
+    )
+    self.clients.claim();
+})
+
+// Intercept fetch requests
+self.addEventListener('fetch', function(evt) {
+    if(evt.request.url.includes('/api/')) {
+        evt.respondWith(
+            caches
+                .open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(evt.request)
+                    .then(response => {
+                        if(response.status === 200) {
+                            cache.put(evt.request.url, response.clone())
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(evt.request)
+                    })
+                })
+                .catch(err => console.log(err))
+        )
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request).catch(function() {
+            return caches.match(evt.request).then(function(response) {
+                if(response) {
+                    return response
+                } else if (evt.request.headers.get('accept').includes('text/html')) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match('/');
+                }
+            })
+        })
+    )
 })
