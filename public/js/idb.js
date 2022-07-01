c4f5c1bd4b25c81efed2ce55e1e49659f2f291e0
Tommy McG <thomasjmcgovern98@gmail.com>
2022-07-01 16:58:35 -0400

    indexdb setup

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,3 +27,59 @@ request.onsuccess = function(event) {
     // log error here
     console.log(event.target.errorCode);
   };
+
+  // This function will be executed if we attempt to submit a new entry and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions 
+  const transaction = db.transaction(['new_entry'], 'readwrite');
+
+  // access the object store for `new_entry`
+  const entryObjectStore = transaction.objectStore('new_entry');
+
+  // add record to your store with add method
+  entryObjectStore.add(record);
+}
+
+function uploadRecord() {
+  // open a transaction on your db
+  const transaction = db.transaction(['new_entry'], 'readwrite');
+
+  // access your object store
+  const entryObjectStore = transaction.objectStore('new_entry');
+
+  // get all records from store and set to a variable
+  const getAll = entryObjectStore.getAll();
+
+  getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_entry'], 'readwrite');
+          // access the new_pizza object store
+          const entryObjectStore = transaction.objectStore('new_entry');
+          // clear all items in your store
+          entryObjectStore.clear();
+
+          alert('All saved entry data has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener('online', uploadRecord);
